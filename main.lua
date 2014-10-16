b0685d7cb496b1321b38be0b3a02a9fce057e509
perry- <alexander.perry@ciber.com>
2014-10-16 20:04:32 +0200

    New game! ..ish

--- main.lua
+++ main.lua
@@ -1,20 +1,59 @@
 function  love.load()
-	points = {}
-	currentColor = {love.math.random(0, 255), love.math.random(0, 255), love.math.random(0, 255)}
+	xPos = 10
+	yPos = love.graphics.getHeight()
+	love.graphics.circle("fill", xPos, yPos, 10)
+
+	balls = {}
+	balls[1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
 end
 
 function love.draw()
-
-	if love.mouse.isDown("l") then
-		points[#points+1] = {love.mouse.getX(), love.mouse.getY(), currentColor}
+	if yPos <= love.graphics.getHeight() then
+		yPos = yPos+5
+	end
+	
+	for i=1, #balls do 
+		if balls[i][2] <= love.graphics.getHeight() then
+			balls[i][2] = balls[i][2]+2
+		end
 	end
 
-	if love.mouse.isDown("r") then
-		currentColor = {love.math.random(0, 255), love.math.random(0, 255), love.math.random(0, 255)}
+	if love.keyboard.isDown("left") and xPos > 0 then
+		xPos = xPos-10
 	end
 
-	for i=1, #points do
-    	love.graphics.circle( "fill", points[i][1], points[i][2], 50, 100 )
-    	love.graphics.setColor(points[i][3])
+	if love.keyboard.isDown("right") and xPos < love.graphics.getWidth() then
+		xPos = xPos+10
 	end
+
+	if love.keyboard.isDown("up") and yPos > 0 then
+		yPos = yPos-10
+	end
+
+	if love.keyboard.isDown("down") and yPos < love.graphics.getHeight() then
+		yPos = yPos+10
+	end
+
+	if love.keyboard.isDown(" ") then
+		love.graphics.setColor(255, 0, 0)
+	end
+
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.circle("fill", xPos, yPos, 10)
+
+	for i=1, #balls do 
+		love.graphics.setColor(0, 255, 0)
+		love.graphics.circle("fill", balls[i][1], balls[i][2], 10)
+	end
+end
+
+function love.update(dt)
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
+	balls[#balls+1] = {love.math.random(10, love.graphics.getWidth()), 0}
 end
